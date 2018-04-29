a802de59d0ce3075af95d01a8db440143d3c0cd6
unknown <spatel.mcsa@gmail.com>
2018-04-29 17:17:36 +0530

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <style type="text/css">
+
+</style>
+<body>
+
+    <div class="container bootstrap snippet">
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="panel panel-default">
+                    <div class="panel-body p-t-0">
+                        <div class="input-group">
+                            <input type="text" id="example-input1-group2" name="example-input1-group2" class="form-control" placeholder="Search">
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-effect-ripple btn-primary"><i class="fa fa-search"></i></button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="panel">
+                    <div class="panel-body p-t-10">
+                        <div class="media-main">
+                            <a class="pull-left" href="#">
+                                <img class="thumb-lg img-circle bx-s" src="https://bootdey.com/img/Content/user_1.jpg" alt="">
+                            </a>
+                            <div class="pull-right btn-group-sm">
+                                <a href="#" class="btn btn-success tooltips" data-placement="top" data-toggle="tooltip" data-original-title="Edit">
+                                    <i class="fa fa-pencil"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger tooltips" data-placement="top" data-toggle="tooltip" data-original-title="Delete">
+                                    <i class="fa fa-close"></i>
+                                </a>
+                            </div>
+                            <div class="info">
+                                <h4>Jonathan Smith</h4>
+                                <p class="text-muted">Graphics Designer</p>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <hr>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-sm-6">
+                <div class="panel">
+                    <div class="panel-body p-t-10">
+                        <div class="media-main">
+                            <a class="pull-left" href="#">
+                                <img class="thumb-lg img-circle bx-s" src="https://bootdey.com/img/Content/user_2.jpg" alt="">
+                            </a>
+                            <div class="pull-right btn-group-sm">
+                                <a href="#" class="btn btn-success tooltips" data-placement="top" data-toggle="tooltip" data-original-title="Edit">
+                                    <i class="fa fa-pencil"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger tooltips" data-placement="top" data-toggle="tooltip" data-original-title="Delete">
+                                    <i class="fa fa-close"></i>
+                                </a>
+                            </div>
+                            <div class="info">
+                                <h4>Jonathan Smith</h4>
+                                <p class="text-muted">Graphics Designer</p>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <hr>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="panel">
+                    <div class="panel-body p-t-10">
+                        <div class="media-main">
+                            <a class="pull-left" href="#">
+                                <img class="thumb-lg img-circle bx-s" src="https://bootdey.com/img/Content/user_2.jpg" alt="">
+                            </a>
+                            <div class="pull-right btn-group-sm">
+                                <a href="#" class="btn btn-success tooltips" data-placement="top" data-toggle="tooltip" data-original-title="Edit">
+                                    <i class="fa fa-pencil"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger tooltips" data-placement="top" data-toggle="tooltip" data-original-title="Delete">
+                                    <i class="fa fa-close"></i>
+                                </a>
+                            </div>
+                            <div class="info">
+                                <h4>Jonathan Smith</h4>
+                                <p class="text-muted">Graphics Designer</p>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <hr>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
